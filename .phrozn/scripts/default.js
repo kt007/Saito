e84925d0738788d4064b3e7f2dd8b3ca3e8b2535
Schlaefer <openmail+sourcecode@siezi.com>
2012-04-20 07:50:57 +0200

    sort order

--- .phrozn/scripts/default.js
+++ .phrozn/scripts/default.js
@@ -1,6 +1,6 @@
 $(document).ready(function() 
     { 
-        $("table").tablesorter({sortList: [[2,1], [1,0]]})
+        $("table").tablesorter({sortList: [[2,1], [1,1]]})
           .addClass('tablesorter table table-striped');
     } 
 );
