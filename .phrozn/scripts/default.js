37f18a30ec862df1e979ef1603d20eb34f431d35
Schlaefer <openmail+sourcecode@siezi.com>
2012-04-20 15:08:21 +0200

    +fwskxgrd; +fix for sorting values

--- .phrozn/scripts/default.js
+++ .phrozn/scripts/default.js
@@ -1,6 +1,9 @@
-$(document).ready(function() 
-    { 
-        $("table").tablesorter({sortList: [[2,1], [1,1]]})
-          .addClass('tablesorter table table-striped');
-    } 
-);
+$(document).ready(function() { 
+  $("table").tablesorter({
+    sortList: [[2,1], [1,1]],
+    textExtraction: function(node) { 
+      return node.innerHTML.replace(/€/g, ''); 
+    }
+  })
+    .addClass('tablesorter table table-striped');
+});
--- scripts/default.js
+++ scripts/default.js
@@ -1,6 +1,11 @@
-$(document).ready(function() 
-    { 
-        $("table").tablesorter({sortList: [[2,1], [1,1]]})
-          .addClass('tablesorter table table-striped');
-    } 
-);
+$(document).ready(function() { 
+  $("table").tablesorter({
+    sortList: [[2,1], [1,1]],
+    textExtraction: function(node) { 
+      // extract data from markup and return it  
+      console.log(node.innerHTML.replace(/[^\d]/g, '')); 
+      return node.innerHTML.replace(/€/g, ''); 
+    }
+  })
+    .addClass('tablesorter table table-striped');
+});
